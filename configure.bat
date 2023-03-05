88ce3d4f586dde10f7a368bf623932e1bf996dae
Jakub Bureš <bures@uzlabina.cz>
2023-03-05 19:04:21 +0100

    Update SDL libraries
    Update SDL to latest version of SDL2. Also updates CMake files and
    configuration script.

--- configure.bat
+++ configure.bat
@@ -1,22 +1,12 @@
+@echo off
 mkdir build
-cmake -S . -B ./build
-@rem -DSDL2_DIR="libraries/SDL2-2.0.16" ^
-@rem -DSDL2_IMAGE_DIR="libraries/SDL2_image-2.0.5" ^
-@rem -DSDL2_MIXER_DIR="libraries/SDL2_mixer-2.0.4" ^
-@rem -DSDL2_TTF_DIR="libraries/SDL2_ttf-2.0.18"
-
-mkdir "build\Debug"
-@rem copy "libraries/SDL2-2.0.16\lib\x64\SDL2.dll" "build\Debug"
-@rem copy "libraries/SDL2_image-2.0.5\lib\x64\*.dll" "build\Debug"
-@rem copy "libraries/SDL2_mixer-2.0.4\lib\x64\*.dll" "build\Debug"
-@rem copy "libraries/SDL2_ttf-2.0.18\lib\x64\*.dll" "build\Debug"
-
-mkdir "build\Release"
-@rem copy "libraries/SDL2-2.0.16\lib\x64\SDL2.dll" "build\Release"
-@rem copy "libraries/SDL2_image-2.0.5\lib\x64\*.dll" "build\Release"
-@rem copy "libraries/SDL2_mixer-2.0.4\lib\x64\*.dll" "build\Release"
-@rem copy "libraries/SDL2_ttf-2.0.18\lib\x64\*.dll" "build\Release"
+cmake -S . -B ./build ^
+-DSDL2_PATH="libraries/SDL2-2.26.3" ^
+-DSDL2_IMAGE_PATH="libraries/SDL2_image-2.6.3" ^
+-DSDL2_MIXER_PATH="libraries/SDL2_mixer-2.6.3" ^
+-DSDL2_TTF_PATH="libraries/SDL2_ttf-2.20.2"
 
 mklink /j "build/assets" "./assets"
 
+echo Solution ready
 pause
